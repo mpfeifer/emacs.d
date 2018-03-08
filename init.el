aee6781bc4c088fa8bf619c581e9799d5fd211a2
Matthias <mpfeifer77@gmail.com>
2018-03-08 16:43:33 +0100

    major modes for all (kinds of xml files)

--- init.el
+++ init.el
@@ -23,6 +23,9 @@
 
 ;; [ custom set variables
 
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
 (load "~/.emacs.d/customx.el")
 
 (setq custom-file "~/.emacs.d/custom.el")
@@ -191,6 +194,8 @@
 
 ;; [ General Emacs Behaviour
 
+(setq auto-window-vscroll nil)
+
 (auto-fill-mode)
 (setq fill-column 72)
 
@@ -340,6 +345,12 @@
 
 ;; ]
 
+;; [ highlight.el
+
+(use-package highlight)
+
+;; ]
+
 ;; [ isearch
 
 ;; 'C-w'     - Select the (rest of the) word the TextCursor is on as
@@ -442,13 +453,12 @@
       delete-old-versions t
       version-control t
       vc-make-backup-files t
-      auto-save-interval 50
+      auto-save-interval 300
       backup-by-copying t
       kept-new-versions 10
       delete-old-versions t
-      vc-make-backup-files t
-      auto-save-file-name-transforms (list (list ".*" auto-save-directory t)))
-
+      vc-make-backup-files t)
+ 
 ;; ]
 
 ;; [ projectile
@@ -478,7 +488,7 @@
 (setq imenu-auto-rescan t
       imenu-use-popup-menu nil
       imenu-space-replacement "-"
-      imenu-sort-function 'imenu--sort-by-name) ;; sort only mouse menu
+      imenu-sort-function 'imenu--sort-by-name) ;; sorts only mouse menu
 
 (defadvice imenu-recenter-advice (after imenu-center activate)
   (recenter-top-bottom 2))
@@ -724,7 +734,8 @@ restore former values for debug-on-error and debug-ignored-errors."
   (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
   (add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
   (add-hook 'js2-mode-hook 'yas-minor-mode)
-  (add-hook 'xml-mode-hook 'yas-minor-mode))
+  (add-hook 'xml-mode-hook 'yas-minor-mode)
+  (add-hook 'nxml-mode-hook 'yas-minor-mode))
 
 ;; ]
 
@@ -791,6 +802,8 @@ restore former values for debug-on-error and debug-ignored-errors."
 ;; ]
 
 ;; [ org mode
+
+;; hydra
 
 ;; TODO: This does not seem to work 
 (use-package org-gcal
@@ -1315,20 +1328,6 @@ and display corresponding buffer in new frame."
 
 ;; [ java mode
 
-(when (getenv "M2_HOME")
-  (message
-   (concat
-    "Overwriting M2_HOME environment variable with custom value \""
-    (expand-file-name mvn-home) "\"")))
-
-(setenv "M2_HOME" (expand-file-name mvn-home))
-(setenv "PATH"
-        (concat (expand-file-name mvn-home) "/bin"
-                path-separator
-                (getenv "PATH")))
-
-(defconst maven (concat (expand-file-name mvn-home) "/bin/mvn"))
-
 (when (getenv "JAVA_HOME")
   (message
    (concat
@@ -1425,47 +1424,6 @@ With prefix argument insert classname with package name. Otherwise omit package 
   (interactive)
   (let* ((classname (completing-read "Class: " java-classes-cache)))
     (java-assert-import classname)))
-
-(defun start-new-web-application (group-id artifact-id version-number)
-  ;; TODO replace maven archetype with copy-file statements
-  ;; include jquery, bootsrap and datatable by default
-  (interactive "MGroup-id: \nMArtifact-id: \nMVersion-number: ")
-  (let* ((project-path web-application-root)
-         (live-buffer-name "*mvn*")
-         (live-buffer (get-buffer-create live-buffer-name))
-         (target-web-xml (concat project-path "/" artifact-id "/src/main/webapp/WEB-INF/web.xml"))
-         (win-edges (window-edges))
-         (this-window-x-min (nth 0 win-edges))
-         (this-window-x-max (nth 2 win-edges))
-         (ww (- this-window-x-max this-window-x-min)))
-    (progn
-      (when (not (file-exists-p project-path))
-        (make-directory project-path t))
-      (switch-to-buffer live-buffer)
-      (when (string= (buffer-name) live-buffer-name)
-        (erase-buffer))
-      (cd project-path)
-      (with-current-buffer live-buffer
-        (term-mode))
-      (call-process maven nil live-buffer-name t "archetype:generate"
-                    (format "-DgroupId=%s" group-id)
-                    (format "-DartifactId=%s" artifact-id)
-                    (format "-Dversion=%s" version-number)
-                    (format "-DarchetypeArtifactId=%s" "maven-archetype-webapp")
-                    (format "-DinteractiveMode=%s" "false") ) 
-      (when (file-exists-p artifact-id)
-        (cd artifact-id))
-      (goto-char (point-min))
-      (when (search-forward "BUILD SUCCESS")
-        (progn
-          (neotree-dir project-path)
-          (other-window 1)
-          (split-window-below -8)
-          (find-file (concat project-path "/" artifact-id "/pom.xml"))
-          (copy-template "web-3.0.xml" target-web-xml
-                            (list 
-                             (list 'DISPLAY-NAME (format "%s %s" artifact-id version-number))))))
-      (goto-char (point-max)) ) ) )
 
 (defun guess-package-name-for-current-buffer ()
   "See if this is a maven project with standard directory layout.
@@ -1598,9 +1556,6 @@ If so calculate pacakge name from current directory name."
 
 ;; [ dired
 
-;; usefull keybindings
-;;
-
 (defun dired-show-only (regexp)
   "Show only files matching REGEXP. To revert buffer to show all files press g."
    (interactive "sFiles to show (regexp): ")
@@ -1652,7 +1607,62 @@ If so calculate pacakge name from current directory name."
             (define-key dired-mode-map (kbd "<backspace>")
               (lambda () (interactive) (find-alternate-file "..")))
             (define-key dired-mode-map (kbd "c") 'dired-2pane-copy-over)
-            (define-key dired-mode-map (kbd "TAB") 'other-window)))
+            (define-key dired-mode-map (kbd "TAB") 'other-window)
+   
+
+
+         (defhydra hydra-dired (:hint nil :color "#268bd2")
+              "
+_+_ mkdir          _v_iew           _m_ark             _(_ details        _i_nsert-subdir    wdired
+_C_opy             _O_ view other   _U_nmark all       _)_ omit-mode      _$_ hide-subdir    C-x C-q : edit
+_D_elete           _o_pen other     _u_nmark           _l_ redisplay      _w_ kill-subdir    C-c C-c : commit
+_R_ename           _M_ chmod        _t_oggle           _g_ revert buf     _e_ ediff          C-c ESC : abort
+_Y_ rel symlink    _G_ chgrp        _E_xtension mark   _s_ort             _=_ pdiff
+_S_ymlink          ^ ^              _F_ind marked      _._ toggle hydra   \\ flyspell
+_r_sync            ^ ^              ^ ^                ^ ^                _?_ summary
+_z_ compress-file  _A_ find regexp
+_Z_ compress       _Q_ repl regexp
+
+T - tag prefix
+"
+              ("\\" dired-do-ispell)
+              ("(" dired-hide-details-mode)
+              (")" dired-omit-mode)
+              ("+" dired-create-directory)
+              ("=" diredp-ediff)         ;; smart diff
+              ("?" dired-summary)
+              ("$" diredp-hide-subdir-nomove)
+              ("A" dired-do-find-regexp)
+              ("C" dired-do-copy)        ;; Copy all marked files
+              ("D" dired-do-delete)
+              ("E" dired-mark-extension)
+              ("e" dired-ediff-files)
+              ("F" dired-do-find-marked-files)
+              ("G" dired-do-chgrp)
+              ("g" revert-buffer)        ;; read all directories again (refresh)
+              ("i" dired-maybe-insert-subdir)
+              ("l" dired-do-redisplay)   ;; relist the marked or singel directory
+              ("M" dired-do-chmod)
+              ("m" dired-mark)
+              ("O" dired-display-file)
+              ("o" dired-find-file-other-window)
+              ("Q" dired-do-find-regexp-and-replace)
+              ("R" dired-do-rename)
+              ("r" dired-do-rsynch)
+              ("S" dired-do-symlink)
+              ("s" dired-sort-toggle-or-edit)
+              ("t" dired-toggle-marks)
+              ("U" dired-unmark-all-marks)
+              ("u" dired-unmark)
+              ("v" dired-view-file)      ;; q to exit, s to search, = gets line #
+              ("w" dired-kill-subdir)
+              ("Y" dired-do-relsymlink)
+              ("z" diredp-compress-this-file)
+              ("Z" dired-do-compress)
+              ("q" nil)
+              ("." nil :color blue))
+            (define-key dired-mode-map (kbd "C-h h") 'hydra-dired/body)
+            ))
 
 (put 'dired-find-alternate-file 'disabled nil)
 
@@ -1663,19 +1673,94 @@ If so calculate pacakge name from current directory name."
 ;; handy: (nxml-balanced-close-start-tag-inline)
 
 (require 'sgml-mode)
-(require 'nxml-mode)
 
-(defun nxml-mode-setup ()
-  (local-set-key (kbd "C-x <return>") 'sgml-close-tag))
+(defun xml-mode-setup ()
+  (local-set-key (kbd "C-x <return>") 'sgml-close-tag)
+  (setq nxml-slash-auto-complete-flag t))
 
 (add-hook 'nxml-mode-hook 'nxml-mode-setup)
 
 (add-to-list 'auto-insert-alist '("pom.xml$" . [ "pom.xml" ]))
 
-(setq xml-modes (list ".*\\.wadl'" ".*\\.xul\\'" ".*\\..rdf\\'" ".*\\.xsd\\'" ".*\\.wsdl\\'"))
+(setq xml-files (list ".*\\.wadl'" ".*\\.xul\\'" ".*\\..rdf\\'" ".*\\.xsd\\'" ".*\\.wsdl\\'"))
 
-(dolist (mode xml-modes)
-  (add-to-list 'auto-mode-alist (cons mode 'xml-mode)))
+(dolist (file xml-modes)
+  (add-to-list 'auto-mode-alist (cons file 'xml-mode)))
+
+;; [ ant mode
+
+(define-derived-mode ant-mode xml-mode
+  "Ant"
+  "Mode for editing ant build.xml files.")
+
+(add-to-list 'auto-mode-alist (cons "build.xml" 'ant-mode))
+
+;; ]
+
+;; [ maven mode
+
+(define-derived-mode maven-mode xml-mode
+  "Maven"
+  "Mode for editing maven pom.xml files.")
+
+(add-to-list 'auto-mode-alist (cons "pom.xml" 'maven-mode))
+
+(when (getenv "M2_HOME")
+  (message
+   (concat
+    "Overwriting M2_HOME environment variable with custom value \""
+    (expand-file-name mvn-home) "\"")))
+
+(setenv "M2_HOME" (expand-file-name mvn-home))
+(setenv "PATH"
+        (concat (expand-file-name mvn-home) "/bin"
+                path-separator
+                (getenv "PATH")))
+
+(defconst maven (concat (expand-file-name mvn-home) "/bin/mvn"))
+
+(defun start-new-web-application (group-id artifact-id version-number)
+  ;; TODO replace maven archetype with copy-file statements
+  ;; include jquery, bootsrap and datatable by default
+  (interactive "MGroup-id: \nMArtifact-id: \nMVersion-number: ")
+  (let* ((project-path web-application-root)
+         (live-buffer-name "*mvn*")
+         (live-buffer (get-buffer-create live-buffer-name))
+         (target-web-xml (concat project-path "/" artifact-id "/src/main/webapp/WEB-INF/web.xml"))
+         (win-edges (window-edges))
+         (this-window-x-min (nth 0 win-edges))
+         (this-window-x-max (nth 2 win-edges))
+         (ww (- this-window-x-max this-window-x-min)))
+    (progn
+      (when (not (file-exists-p project-path))
+        (make-directory project-path t))
+      (switch-to-buffer live-buffer)
+      (when (string= (buffer-name) live-buffer-name)
+        (erase-buffer))
+      (cd project-path)
+      (with-current-buffer live-buffer
+        (term-mode))
+      (call-process maven nil live-buffer-name t "archetype:generate"
+                    (format "-DgroupId=%s" group-id)
+                    (format "-DartifactId=%s" artifact-id)
+                    (format "-Dversion=%s" version-number)
+                    (format "-DarchetypeArtifactId=%s" "maven-archetype-webapp")
+                    (format "-DinteractiveMode=%s" "false") ) 
+      (when (file-exists-p artifact-id)
+        (cd artifact-id))
+      (goto-char (point-min))
+      (when (search-forward "BUILD SUCCESS")
+        (progn
+          (neotree-dir project-path)
+          (other-window 1)
+          (split-window-below -8)
+          (find-file (concat project-path "/" artifact-id "/pom.xml"))
+          (copy-template "web-3.0.xml" target-web-xml
+                            (list 
+                             (list 'DISPLAY-NAME (format "%s %s" artifact-id version-number))))))
+      (goto-char (point-max)) ) ) )
+
+;; ]
 
 ;; ]
 
@@ -1952,7 +2037,7 @@ If so calculate pacakge name from current directory name."
 ;; For ease of use: Make sure that ssh access is granted via public key
 
 (setq tramp-default-method "ssh"
-      tramp-auto-save-directory "~/.emacs.d/tramp-auto-save/")
+      tramp-auto-save-directory "~/.emacs.d/auto-save/")
 
 ;; ]
 
@@ -2213,7 +2298,7 @@ If so calculate pacakge name from current directory name."
 (use-package company
   :config
   ;; see company-backends for company backends
-  (make-variable-buffer-local 'company-backends)
+  ;; (make-variable-buffer-local 'company-backends)
   (require 'company-template))
 
 ;; ]
@@ -2322,6 +2407,9 @@ If so calculate pacakge name from current directory name."
 ;; C-x p s    save bookmars list
 
 ;; ]
+
+(setq gc-cons-threshold 33554432
+      gc-cons-percentage 0.1)
 
 (notify "[Emacs] init.el fully loaded")
 
